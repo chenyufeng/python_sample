160668af3477794f1901d9dc34b50746aa738e11
chenyufeng <jimmy.chen.88@gmail.com>
2016-10-26 19:14:29 +0800

    input & output test

--- test1.py
+++ test1.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python
+from  datetime import date
+
+name=raw_input('please input your name: ')
+print 'hello', name
+age=raw_input('please input your age: ')
+print 'your birth year: ', date.today().year-int(age)
